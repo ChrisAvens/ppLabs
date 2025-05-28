e8e0d5b0d156ec120f151d84105c7b600322f7f3
ChrisAvens <120141264+ChrisAvens@users.noreply.github.com>
2025-05-28 16:14:51 -0600

    Update main.py
    added comments to main function

--- ParkJamPP/main.py
+++ ParkJamPP/main.py
@@ -3,9 +3,11 @@ from PyQt6.QtWidgets import QApplication
 from ui.game_window import GameWindow
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv) # Creates app instance
+    # Initiates window and makes it visible
     window = GameWindow()
     window.show()
+    # QT event loop
     sys.exit(app.exec())
 
 if __name__ == "__main__":
